d6a73f751df0ad79a7aa5a607b739abbba85fef1
Skyz03 <aakibdiscoo@gmail.com>
2022-02-22 16:11:12 +0545

    Updates in the navigation with the cross element

--- app.js
+++ app.js
@@ -2,16 +2,24 @@ const navItems = document.querySelectorAll(".navigation__item");
 
 const button = document.getElementsByClassName("icon")[0];
 
+const iconBars = document.getElementById("icon-bars");
 
+const iconCross = document.getElementById("icon-cross");
 
 
 function navToggle() {
 
   navItems.forEach(navItem => {
     navItem.classList.toggle("active");
-  })
 
+    if (navItem.classList.contains("active")) {
+      iconCross.style.display = "block";
+      iconBars.style.display = "none";
+    } else {
+      iconCross.style.display = "none";
+      iconBars.style.display = "block";
+    }
+  });
 }
-
 
 button.addEventListener("click", navToggle);
